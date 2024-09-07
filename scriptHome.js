7b3dc457ba42c36fd55ec8ff142f42d1a879543f
Mram <144263287+Mram-ui@users.noreply.github.com>
2024-09-07 19:22:32 +0300

    Delete scriptHome.js

--- scriptHome.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to change the header background color on scroll
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 50) { // Change 50 to the number of pixels you want to scroll before changing the color
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
