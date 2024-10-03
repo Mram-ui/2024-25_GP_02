dcc45add9413033ef9e1a866c445a389895391ff
BashairAlsadhan <bashair.sadhan@gmail.com>
2024-10-03 12:07:17 +0300

    adding yolo8

--- WebsitePages/Back-End/PHP/accountDetails.php
+++ WebsitePages/Back-End/PHP/accountDetails.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account Details</title>
-</head>
-<body>
-    
-</body>
+<html lang="es" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../../Front-End/CSS/login.css"><link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header class="header">
+      <div class="logo">
+          <a href="home.html"><img src="../../images/Logo2.png" alt="Company Logo"></a>
+      </div>
+      
+  </header>
+
+    <div class="main">
+      <div class="container" >
+            
+            <h2 class="title"> Account details</h2>
+            <div class="img">
+                <img src="logo.png" alt="Company Logo" class="logo">
+            </div>
+            <div class="account-details">
+
+                <h3>company name</h3>
+                <p>Email: <span id="email">user@example.com</span></p>
+                <div class="button">
+                        <button id="editEmail">Edit</button>
+                        <button id="resetPassword">Reset Password</button>
+                </div>
+            </div>
+          
+        </div>
+      
+      
+    </div>
+   
+  </body>
 </html>
---
+++ Yolo/asaa.py
@@ -0,0 +1,48 @@
+import cv2
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO('yolov8n.pt')  # we can also use yolov8s.pt, yolov8m.pt, etc. for larger models
+
+# Open the webcam
+cap =cv2.VideoCapture("rtsp://Raqeeb1:raqeebCCTV2025@192.168.8.46:554/stream1")
+
+
+if not cap.isOpened():
+    print("Error: Could not open video stream from camera.")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Detect people in the frame
+    results = model(frame, agnostic_nms=True,conf=0.5) # Remove the double detection, adding threshod
+
+    # Filter detections to only include people (class ID for person in COCO dataset is 0)
+    people_count = 0
+    for result in results:
+        for box in result.boxes:
+            if int(box.cls) == 0:  # Check if the detected class is person
+                people_count += 1
+                # Extract box coordinates
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                # Draw bounding box arond the person
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    # Display the number of people in the frame
+    text = f"People count: {people_count}"
+    cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    # Display the resulting frame with detections
+    cv2.imshow('YOLOv8 People Detection', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        break
+
+# release the capture and close the window
+cap.release()
+cv2.destroyAllWindows()
