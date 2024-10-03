3f4a1ae41223d06e89ccaf616256fa1df79099e3
ramafahad <rama.alshebel@gmail.com>
2024-10-03 03:20:28 +0300

    add event, add camera. forgot password

---
+++ WebsitePages/Back-End/PHP/addCamera.php
@@ -0,0 +1,43 @@
+<html lang="es" dir="ltr">
+
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Add a Camera</title>
+    <link rel="stylesheet" type="text/css" href="../../Front-End/CSS/boxes.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo">
+            <a href="home.html"><img src="../../images/Logo2.png" alt="Company Logo"></a>
+        </div>
+        <nav>
+            <a href="#about">account icon</a>
+            <a href="#contact">Contact Us</a>
+
+        </nav>
+    </header>
+
+    <div class="main">
+        <a id='arrow' href="login.html"><i  class="fa fa-chevron-left" style="color: #003f91; font-size: 30px; justify-self: end;"></i></a>
+        <h2 class="title" >Add a Camera</h2>
+        <form id='addcam' class="form" method="POST" action="">
+            <label for="cameraName">Camera Name:</label> <input name="cameraName" class="form__input" type="text" placeholder="Camera 1" required>
+            <label for="cameraIP">Camera IP Address:</label> <input name="cameraIP" class="form__input" type="text" placeholder="000.000.0.000" required>
+            <label for="portNo">Port Number:</label> <input name="portNo" class="form__input" type="text" placeholder="0" required>
+            <label for="stream">Streaming Channel:  </label><input name="stream" class="form__input" type="text" placeholder="stream1" required>
+            <label for="cameraUsername">Camera Username:</label><input name="cameraUsername" class="form__input" type="text" placeholder="Username" required>
+            <label for="cameraPassword">Camera Password:</label><input name="cameraPassword" class="form__input" type="text" placeholder="Password" required>
+
+            <button class="form__button button submit">ADD CAMERA</button>
+          </form>
+
+
+    </div>
+</body>
+
+</html>
--- WebsitePages/Back-End/PHP/addEvent.php
+++ WebsitePages/Back-End/PHP/addEvent.php
@@ -1,11 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="es" dir="ltr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adding event </title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Add Event</title>
+    <link rel="stylesheet" type="text/css" href="../../Front-End/CSS/boxes.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 </head>
+
 <body>
-    
+    <header class="header">
+        <div class="logo">
+            <a href="home.html"><img src="../../images/Logo2.png" alt="Company Logo"></a>
+        </div>
+        <nav>
+            <a href="#about">account icon</a>
+            <a href="#contact">Contact Us</a>
+
+        </nav>
+    </header>
+
+    <div class="main">
+        <a id='arrow' href="login.html"><i  class="fa fa-chevron-left" style="color: #003f91; font-size: 30px; justify-self: end;"></i></a>
+        <h2 class="title" >Add Event</h2>
+        <form id='addEvent' class="form" method="POST" action="">
+            <label for="eventName">Event Name:</label> <input name="eventName" class="form__input" type="text" placeholder="Name" required> <br>
+            <label for="eventLocation">Event Location:</label> <input name="eventLocation" class="form__input" type="text" placeholder="Exhibition center, Riyadh" required> <br>
+
+            <div id="times">
+                <div class="timeBlocks"><label for="startDate">Start Date:</label>  <input name="startDate" class="form__input time" type="date"  required> </div>
+                <div class="timeBlocks"> <label for="startTime">Start Time:  </label> <input name="startTime" class="form__input time" type="time"  required> </div>
+                 <br>
+                 <div class="timeBlocks"><label  for="endDate">End Date:  </label> <input name="endDate" class="form__input time" type="date"  required> </div>
+                 <div class="timeBlocks"> <label for="endTime">End Time:  </label>  <input name="endTime" class="form__input time" type="time"  required> </div>
+            </div>
+
+            <div class="hall">
+                <label for="hallName">Hall Name:</label><input name="hallName" class="form__input" type="text" placeholder="main hall" required>
+                <label for="hallCamera">Hall Name:</label>
+                <select name="hallCamera">
+                    <option>Camera 1</option>
+                    <option>Camera 2</option>
+                </select>
+                <label for="hallThreshold">Hall Threshold:</label><input name="hallThreshold" class="form__input" type="text" placeholder="##" required>
+            </div>
+            <br>
+            <button class="form__button button submit">ADD EVENT</button>
+          </form>
+
+
+    </div>
 </body>
+
 </html>
