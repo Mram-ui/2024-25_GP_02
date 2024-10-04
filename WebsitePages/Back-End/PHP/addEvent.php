0da98cf98c1e2093f20cd901f2e01cac09600ce4
Mram <144263287+Mram-ui@users.noreply.github.com>
2024-10-04 16:23:49 +0300

    Update addEvent.php

--- WebsitePages/Back-End/PHP/addEvent.php
+++ WebsitePages/Back-End/PHP/addEvent.php
@@ -13,7 +13,7 @@
 <body>
     <header class="header">
         <div class="logo">
-            <a href="UserHome.php"><img src="../../images/Logo2.png" alt="Company Logo"></a>
+            <a href="../../Back-End/PHP/UserHome.php"><img src="../../images/Logo2.png" alt="Company Logo"></a>
         </div>
     </header>
 
