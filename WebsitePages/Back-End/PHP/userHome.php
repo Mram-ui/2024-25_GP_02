541b5440cba0d71b79096a455c53b0ad87b036cb
Mram <144263287+Mram-ui@users.noreply.github.com>
2024-10-16 16:59:23 +0300

    Update userHome.php

--- WebsitePages/Back-End/PHP/userHome.php
+++ WebsitePages/Back-End/PHP/userHome.php
@@ -49,7 +49,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
-        <title>Organizer</title>
+        <title>Events</title>
         <style>
             /* ---- Html Style ---- */
             * {
