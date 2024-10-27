c6386952d4bd93a261eb93b8475f31876e091942
Mram <144263287+Mram-ui@users.noreply.github.com>
2024-10-27 18:43:14 +0300

    Update session.php

--- WebsitePages/Back-End/PHP/session.php
+++ WebsitePages/Back-End/PHP/session.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['CompanyID'])) {
     // Allow access to login and signup pages
     if (basename($_SERVER['PHP_SELF']) !== 'login.php' && basename($_SERVER['PHP_SELF']) !== 'signup.php') {
         // Redirect to login page
-        header('Location: ../../Front-End/login.html');
+        header('Location: ./../Front-End/HTML/login.html');
         exit();
     }
 }
