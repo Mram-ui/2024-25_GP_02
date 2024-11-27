382e924454cf0e6dce6f7267c4ec579a2e3cc643
Mram <144263287+Mram-ui@users.noreply.github.com>
2024-11-27 15:01:35 +0300

    Update forgetPassProcessing.php

--- WebsitePages/Back-End/PHP/forgetPassProcessing.php
+++ WebsitePages/Back-End/PHP/forgetPassProcessing.php
@@ -1,182 +1,61 @@
 <?php
+    session_start();
 
-session_start();
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "raqeebdb";
 
-include("dbConnection.php");
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if(isset($_POST['email'])){
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-    if (empty($_POST["email"])) {
-        echo '
-            <a href="../../Back-End/PHP/index.php"><img id="logo" src="../../images/Logo2.png" alt="Company Logo"></a>
-            <a id="arrow" href="../../Front-End/HTML/login.html"><i  class="fa fa-chevron-left" style="color: #003f91; font-size: 30px; float: left; margin-left: 20px; margin-top: 20px;"></i></a>
-            <h2 id="forgotTitle" class="title" style="margin-top: 3%;">FORGET YOUR PASSWORD?</h2>
-            <br> <br> 
-            <div id="forgotDesc" > 
-                <p style="font-size: 17px;">Please enter your registered email address and<br> we will send you an email to reset your password</p>
-                <form class="form" id="ForgotPasswordForm">
-                    <br>
-                    <input name="femail" class="form__input" id="email" type="email" placeholder="Email" required>
-                  <div class="form-message" id="msg" style="color:red;">Please enter your email to retreive your account</div>
-                    <button type="submit" class="form__button button submit"> SEND</button>
-                  </form>
-            </div>';
-    } else {
-        $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    if(isset($_POST['email'])) {
+        if (empty($_POST["email"])) {
+            echo "<div class='error-message' style='color:red;'>Please enter your email to retrieve your account.</div>";
+        } else {
+            $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo '
-            <a href="../../Back-End/PHP/index.php"><img id="logo" src="../../images/Logo2.png" alt="Company Logo"></a>
-            <a id="arrow" href="../../Front-End/HTML/login.html"><i  class="fa fa-chevron-left" style="color: #003f91; font-size: 30px; float: left; margin-left: 20px; margin-top: 20px;"></i></a>
-            <h2 id="forgotTitle" class="title" style="margin-top: 3%;">FORGET YOUR PASSWORD?</h2>
-            <br> <br> 
-            <div id="forgotDesc" > 
-                <p style="font-size: 17px;">Please enter your registered email address and<br> we will send you an email to reset your password</p>
-                <form class="form" id="ForgotPasswordForm">
-                    <br>
-                    <input name="femail" class="form__input" id="email" type="email" placeholder="Email" required>
-                    <div class="form-message" id="msg" style="color:red;">Invalid email format</div>
-                    <button type="submit" class="form__button button submit"> SEND</button>
-                  </form>
-            </div>';
-        }
-        else{
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "<div class='error-message' style='color:red;'>Please enter a valid email address!</div>";
+            } else {
+                $query = "SELECT * FROM company WHERE Email='$email'";
+                $row = mysqli_query($conn, $query);
 
-            $query="SELECT * FROM company WHERE Email='$email'";
-            $row=mysqli_query($connection,$query);
+                if (mysqli_num_rows($row) > 0) {
+                    $token = uniqid(md5(time()));
+                    $_SESSION["token"] = $token;
+                    $_SESSION["tokenTimeStamp"] = time();
+                    $_SESSION["email"] = $email;
 
-            if(mysqli_num_rows($row)>0){
-                $token=uniqid(md5(time()));
-    
-                $_SESSION["token"]=$token;
-                $_SESSION["tokenTimeStamp"] = time();
-                $_SESSION["email"]=$email;
-    
-                /**
-                 * This example shows settings to use when sending via Google's Gmail servers.
-                 * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
-                 */
-                
-                //SMTP needs accurate times, and the PHP time zone MUST be set
-                //This should be done in your php.ini, but this is how to do it if you don't have access to that
-                date_default_timezone_set('Asia/Riyadh');
-                
-                require '../../PHPMailer/PHPMailerAutoload.php';
-                
-                //Create a new PHPMailer instance
-                $mail = new PHPMailer;
-                
-                //Tell PHPMailer to use SMTP
-                $mail->isSMTP();
-                
-                //Enable SMTP debugging
-                // 0 = off (for production use)
-                // 1 = client messages
-                // 2 = client and server messages
-                $mail->SMTPDebug = 0;
-                
-                //Ask for HTML-friendly debug output
-                $mail->Debugoutput = 'html';
-                
-                //Set the hostname of the mail server
-                $mail->Host = 'smtp.gmail.com';
-                // use
-                // $mail->Host = gethostbyname('smtp.gmail.com');
-                // if your network does not support SMTP over IPv6
-                
-                //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-                $mail->Port = 587;
-                
-                //Set the encryption system to use - ssl (deprecated) or tls
-                $mail->SMTPSecure = 'tls';
-                
-                //Whether to use SMTP authentication
-                $mail->SMTPAuth = true;
-                
-                //Username to use for SMTP authentication - use full email address for gmail
-                $mail->Username = "raqeeb.project@gmail.com";
-                
-                //Password to use for SMTP authentication
-                $mail->Password = "vjrd xeyj lrlw ymcf"; //due to upload to github password is not there
-                
-                //Set who the message is to be sent from
-                $mail->setFrom('raqeeb.project@gmail.com', 'Raqeeb');
-                
-                //Set an alternative reply-to address
-                $mail->addReplyTo('no-replyto@webscript.info', 'First Last');
-                
-                //Set who the message is to be sent to
-                $mail->addAddress($email, 'John Doe');
-                
-                //Set the subject line
-                $mail->Subject = 'Forgot password';
-                
-                //Read an HTML message body from an external file, convert referenced images to embedded,
-                //convert HTML into a basic plain-text alternative body
-                $mail->msgHTML("<!DOCTYPE html> <html> <body>Greetings, <br> You have requested to reclaim your account, if the request was made by you, please click <a href='http://localhost/2024-25_GP_02/WebsitePages/Back-End/PHP/reclaimAccount.php?token=$token&email=$email'>Here</a>. Please note that the reset link will expire in 10 minutes. <br> If you did not make this request, please ignore this email.</body> </html>");
-                
-                //Replace the plain text body with one created manually
-                $mail->AltBody = 'This is a plain-text message body';
-                
-                //Attach an image file
-                //$mail->addAttachment('images/phpmailer_mini.png');
-                
-                //send the message, check for errors
-                if (!$mail->send()) {
-                    echo "
-                    <a href='../../Back-End/PHP/index.php'><img id='logo' src='../../images/Logo2.png' alt='Company Logo'></a>
-                    <a id='arrow' href='../../Front-End/HTML/login.html'><i  class='fa fa-chevron-left' style='color: #003f91; font-size: 30px; float: left; margin-left: 20px; margin-top: 20px;'></i></a>
-                    <h2 id='forgotTitle' class='title' style='margin-top: 3%;'>FORGET YOUR PASSWORD?</h2>
-                    <br> <br> 
-                    <div id='forgotDesc' > 
-                        <p style='font-size: 17px;'>Please enter your registered email address and<br> we will send you an email to reset your password</p>
-                        <form class='form' id='ForgotPasswordForm'>
-                            <br>
-                            <input name='femail' class='form__input' id='email' type='email' placeholder='Email' required>
-                            <div class='errorMessage' id='EmailNotValid'></div>
-                            <div class='form-message' id='msg' style='color:red;'> Mailer Error: . $mail->ErrorInfo </div>
+                    date_default_timezone_set('Asia/Riyadh');
 
-                            <button type='submit' class='form__button button submit'> SEND</button>
-                          </form>
-                    </div>";
+                    require '../../Back-End/PHP/PHPMailer/PHPMailerAutoload.php';
+                    $mail = new PHPMailer;
+                    $mail->isSMTP();
+                    $mail->Host = 'smtp.gmail.com';
+                    $mail->Port = 587;
+                    $mail->SMTPSecure = 'tls';
+                    $mail->SMTPAuth = true;
+                    $mail->Username = "raqeeb.project@gmail.com";
+                    $mail->Password = "vjrd xeyj lrlw ymcf";
+                    $mail->setFrom('raqeeb.project@gmail.com', 'Raqeeb');
+                    $mail->addAddress($email, 'User');
+                    $mail->Subject = 'Forgot password';
+                    $mail->msgHTML("<html><body>Greetings, <br> You have requested to reclaim your account, if the request was made by you, please click <a href='http://localhost/2024-25_GP_02-main/WebsitePages/Back-End/PHP/reclaimAccount.php?token=$token&email=$email'>Here</a>. Please note that the reset link will expire in 10 minutes. <br> If you did not make this request, please ignore this email.</body></html>");
+                    $mail->AltBody = 'This is a plain-text message body';
+
+                    if (!$mail->send()) {
+                        echo "<div class='error-message' style='color:red;'>Mailer Error: {$mail->ErrorInfo}</div>";
+                    } else {
+                        echo "<div class='success-message' style='color:green;'>Email sent! Please check your inbox to reset your password.</div>";
+                    }
                 } else {
-                    echo " <a href='../../Back-End/PHP/index.php'> <img id='logo' src='../../images/Logo2.png' alt='Company Logo'></a>
-                    <a id='arrow' href='../../Front-End/HTML/login.html'><i  class='fa fa-chevron-left' style='color: #003f91; font-size: 30px; float: left; margin-left: 18px; margin-top: 20px;'></i></a> <br><br><br>
-
-                    <img src='../../images/mail.png' style='height:180px; width:auto;'>  <br>  <h2 class='title' style='margin-top: 3%;'>Email sent!</h2>  <p style='font-size:20px;'>Please make sure you open the link on the <br> same browser you made the request from.</p>";
+                    echo "<div class='form-message' id='msg' > <p style='color:red;'> No account was found with that email</p> <a href='../../Front-End/HTML/login.html'>Sign up instead </a> </div>";
                 }
-                
-
-
-
-
-        
-            }else{
-                echo "
-                <a href='../../Back-End/PHP/index.php'><img id='logo' src='../../images/Logo2.png' alt='Company Logo'></a>
-                <a id='arrow' href='../../Front-End/HTML/login.html'><i  class='fa fa-chevron-left' style='color: #003f91; font-size: 30px; float: left; margin-left: 20px; margin-top: 20px;'></i></a>
-                <h2 id='forgotTitle' class='title' style='margin-top: 3%;'>FORGET YOUR PASSWORD?</h2>
-                <br> <br> 
-                <div id='forgotDesc' > 
-                    <p style='font-size: 17px;'>Please enter your registered email address and<br> we will send you an email to reset your password</p>
-                    <form class='form' id='ForgotPasswordForm'>
-                        <br>
-                        <input name='femail' class='form__input' id='email' type='email' placeholder='Email' required>
-                      <div class='form-message' id='msg' > <p style='color:red;'> No account was found with that email</p> <a href='../../Front-End/HTML/login.html'>Sign up instead </a> </div> 
-                        <button type='submit' class='form__button button submit'> SEND</button>
-                      </form>
-                      <br>
-                </div>";
             }
-
         }
-
-
     }
-
-    }
-
-
-
-
 ?>
