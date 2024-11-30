dc5fd978cb0e8acc151e96060db10e92f47d7725
ramafahad <rama.alshebel@gmail.com>
2024-11-30 20:59:23 +0300

    moving phpmailer

--- WebsitePages/Back-End/PHP/contactUs.php
+++ WebsitePages/Back-End/PHP/contactUs.php
@@ -20,7 +20,7 @@
                 //This should be done in your php.ini, but this is how to do it if you don't have access to that
                 date_default_timezone_set('Asia/Riyadh');
                 
-                require '../../PHPMailer/PHPMailerAutoload.php';
+                require 'PHPMailer/PHPMailerAutoload.php';
                 
                 //Create a new PHPMailer instance
                 $mail = new PHPMailer;
