f81fcbd2e2dc510f7d31cbcc84e33eb61f207a96
Mram <144263287+Mram-ui@users.noreply.github.com>
2025-01-30 11:55:37 +0300

    Update resetpassword.php

--- WebsitePages/Back-End/PHP/resetpassword.php
+++ WebsitePages/Back-End/PHP/resetpassword.php
@@ -1,50 +1,50 @@
 <?php
-include("dbConnection.php");
-session_start();
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "raqeebdb";
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if(!isset($_POST['oldpass'])){
-$email=$_POST['email'];
-$pass = password_hash($_POST['newpass'], PASSWORD_DEFAULT);
+    if (!isset($_POST['oldpass'])) {
+        $email = $_POST['email'];
+        $pass = password_hash($_POST['newpass'], PASSWORD_DEFAULT);
 
-$query="UPDATE company SET Password='$pass' WHERE Email='$email'";
-$res=mysqli_query($connection,$query);
-if($res){
-    //session_destroy();
-    echo "Password updated successfully, please <a href='../../Front-End/HTML/login.html'>Log in</a>";
-}
-else{
-     echo "could not update password, please try again";
-}
-}
+        $query = "UPDATE company SET Password='$pass' WHERE Email='$email'";
+        $res = mysqli_query($conn, $query);
 
-else{
-    $CompanyID = $_SESSION['CompanyID'] ?? null;
-    $query = 'SELECT * FROM company WHERE CompanyID=' . $CompanyID;
-    $row = mysqli_fetch_assoc(mysqli_query($connection, $query));
-
-
-        if ($row) {
-            if (password_verify($_POST['oldpass'], $row['Password'])) {
-                $pass = password_hash($_POST['newpass'], PASSWORD_DEFAULT);
-                $query="UPDATE company SET Password='$pass' WHERE CompanyID=" . $CompanyID;
-                $res=mysqli_query($connection,$query);
-                if($res){
-                    echo '1';
-
-                    }
-
-                else{
-                        echo '2';
-                    }
-                }
-            else{
-                echo '3';
-
-            }
+        if ($res) {
+            echo "Password updated successfully, please <a href='../../Front-End/HTML/login.html'>Log in</a>";
+        } else {
+            echo "Could not update password, please try again.";
         }
-        else{
-            echo '2';
+    } else {
+        session_start(); 
+        $CompanyID = $_SESSION['CompanyID'] ?? null;
+
+        if ($CompanyID) {
+            $query = "SELECT * FROM company WHERE CompanyID=" . $CompanyID;
+            $row = mysqli_fetch_assoc(mysqli_query($conn, $query));
+
+            if ($row) {
+                if (password_verify($_POST['oldpass'], $row['Password'])) {
+                    $newPass = password_hash($_POST['newpass'], PASSWORD_DEFAULT);
+                    $query = "UPDATE company SET Password='$newPass' WHERE CompanyID=" . $CompanyID;
+                    $res = mysqli_query($conn, $query);
+
+                    if ($res) {
+                        echo '1'; // Password changed successfully
+                    } else {
+                        echo '2'; // Error in updating password
+                    }
+                } else {
+                    echo '3'; // Incorrect old password
+                }
+            } else {
+                echo '2'; // Error: No such user found
+            }
+        } else {
+            echo '2'; // Error: No session found
         }
     }
-
+?>
